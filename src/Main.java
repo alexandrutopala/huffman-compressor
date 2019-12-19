be528aa7fd4b754973999c0632e1a58c88a7a4d4
Andrei Rotaru <andreirotaru1998@gmail.com>
2019-12-19 16:24:28 +0200

    - just the encode part

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
+import compress.implementation.HuffmanDictionary;
+import compress.implementation.HuffmanTextDictionary;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        HuffmanDictionary dictionary = new HuffmanTextDictionary();
+
+//
+//         Just for testing
+//
+//       Map<Character, byte[]> mapa = dictionary.compute("ana are mere");
+//       mapa.forEach((k,v) -> {
+//           System.out.println(k);
+//           System.out.println(Arrays.toString(v));
+//       } );
 
     }
 }
