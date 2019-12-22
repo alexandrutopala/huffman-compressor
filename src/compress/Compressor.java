e9a582f57f932cdd85af88c3152dba421884c5ad
Alexandru Topala <alexandru.topala@ab4.systems>
2019-12-22 12:03:04 +0200

    written compression

--- src/compress/Compressor.java
+++ src/compress/Compressor.java
@@ -5,4 +5,24 @@ public interface Compressor<T> {
     byte[] compress(T t);
 
     T decompress(byte[] compressed);
+
+    static Byte[] wrap(byte[] arr) {
+        Byte[] wrapped = new Byte[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            wrapped[i] = arr[i];
+        }
+
+        return wrapped;
+    }
+
+    static byte[] unwrap(Byte[] arr) {
+        byte[] unwrapped = new byte[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            unwrapped[i] = arr[i];
+        }
+
+        return unwrapped;
+    }
 }
--- src/compress/implementation/HuffmanTextCompressor.java
+++ src/compress/implementation/HuffmanTextCompressor.java
@@ -2,6 +2,8 @@ package compress.implementation;
 
 import compress.Compressor;
 
+import java.nio.ByteBuffer;
+import java.util.Arrays;
 import java.util.Map;
 
 class HuffmanTextCompressor implements Compressor<String> {
@@ -9,11 +11,52 @@ class HuffmanTextCompressor implements Compressor<String> {
     private HuffmanTextDictionary dictionaryComputer = new HuffmanTextDictionary();
 
     @Override
-    public byte[] compress(String s) {
-        Map<Character, byte[]> dictionary = dictionaryComputer.compute(s);
+    public byte[] compress(String message) {
+        Map<Character, byte[]> dictionary = dictionaryComputer.compute(message);
 
+        byte[] encodedDictionary = encodeDictionary(dictionary);
+        byte[] encodedMessage = encodeMessage(message, dictionary);
 
-        return new byte[0];
+        ByteBuffer buffer = ByteBuffer.allocate(encodedDictionary.length + encodedMessage.length);
+
+        buffer.put(encodedDictionary);
+        buffer.put(encodedMessage);
+
+        return buffer.array();
+    }
+
+    private byte[] encodeMessage(String message, Map<Character, byte[]> dictionary) {
+        Byte[] encoded = message.chars()
+                .mapToObj(letter -> (char) letter)
+                .map(dictionary::get)
+                .map(Compressor::wrap)
+                .flatMap(Arrays::stream)
+                .toArray(Byte[]::new);
+
+        return Compressor.unwrap(encoded);
+    }
+
+    private byte[] encodeDictionary(Map<Character, byte[]> dictionary) {
+        int capacity = computeBufferCapacity(dictionary);
+
+        ByteBuffer buffer = ByteBuffer.allocate(capacity);
+
+        buffer.putShort((short) dictionary.size());
+
+        dictionary.forEach((symbol, bytes) -> {
+            buffer.putChar(symbol);
+            buffer.putShort((short) bytes.length);
+            buffer.put(bytes);
+        });
+
+        return buffer.array();
+    }
+
+    private int computeBufferCapacity(Map<Character, byte[]> dicitonary) {
+        return 2 // for total entries count (represented on 2B)
+             + dicitonary.size() * 2 // for byte counter for each symbol (represented on 2B)
+             + dicitonary.size() * 2 // for each encoded symbol (represented on 2B)
+             + dicitonary.values().stream().mapToInt(arr -> arr.length).sum(); // for effective encoding byte
     }
 
     @Override
